4cb1c9fc3ede8dc2420fcfef1ca65cb3ce43b22b
SHAHZAIB-AQADIR <shahzebaqadir@gmail.com>
2025-12-31 01:14:49 +0500

    Fix image case for Vercel

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,12 +1,12 @@
 import GameCard from "@/app/components/GameCard/GameCard";
 
 const games = [
-  { title: "VALORANT", img: "/valorant.png", link: "/games/valorant" },
-  { title: "FORZA", img: "/forza.png", link: "/games/forza" },
+  { title: "VALORANT", img: "/VALORANT.png", link: "/games/valorant" },
+  { title: "FORZA", img: "/Forza.png", link: "/games/forza" },
   { title: "CS-GO", img: "/csgo.png", link: "/games/csgo" },
-  { title: "GTA-V", img: "/gta5.png", link: "/games/gta5" },
-  { title: "Fortnite", img: "/fortnite.png", link: "/games/fortnite" },
-  { title: "ASPHALT", img: "/asphalt.png", link: "/games/asphalt" },
+  { title: "GTA-V", img: "/GTA5.png", link: "/games/gta5" },
+  { title: "Fortnite", img: "/Fortnite.png", link: "/games/fortnite" },
+  { title: "ASPHALT", img: "/ASPHALT.png", link: "/games/asphalt" },
 ];
 
 export default function GamesPage() {
